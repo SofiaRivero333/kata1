185acee3239a06db846012d7e70910bc58b58a33
SofiaRivero333 <gasoda.16@gmail.com>
2026-09-22 18:39:39 +0100

    Project kata1: Second commit, Person and Main

---
+++ src/main/java/software/ulpgc/kata/Person.java
@@ -0,0 +1,23 @@
+package software.ulpgc.kata;
+
+public class Person {
+    private final String name;
+    private final int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean adult(){
+        return age >= 19;
+    }
+}
