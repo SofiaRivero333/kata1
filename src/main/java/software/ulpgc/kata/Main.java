4c4bfa0b1ce0017a8499e20812ddd3c6e3b9fe2d
SofiaRivero333 <gasoda.16@gmail.com>
2026-09-23 16:35:17 +0100

    kata 1 repetition 1: prepare structure

--- src/main/java/software/ulpgc/kata/Main.java
+++ src/main/java/software/ulpgc/kata/Main.java
@@ -1,13 +1,5 @@
 package software.ulpgc.kata;
 
 public class Main {
-    public static void main(String[] args) {
-        Person person = new Person("Sofia", 20);
-
-        System.out.println("Nombre: " + person.getName());
-        System.out.println("Edad: " + person.getAge());
-        System.out.println("Adulto: " + person.isAdult());
-
-    }
 
 }
